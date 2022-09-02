fd283b36689959a2422c1d7b0063d8286a092d66
Murphygrace22 <grace.murphy22@yahoo.com>
2022-09-02 17:49:41 -0400

    Initial commit

---
+++ first.txt.py
@@ -0,0 +1,11 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red161\green103\blue230;\red31\green31\blue36;\red255\green255\blue255;
+}
+{\*\expandedcolortbl;;\csgenericrgb\c63137\c40392\c90196;\csgenericrgb\c12054\c12284\c14131;\csgenericrgb\c100000\c100000\c100000\c85000;
+}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab593
+\pard\tx593\pardeftab593\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf2 \cb3 print\cf4 (\'93Hello World!\'94)}
